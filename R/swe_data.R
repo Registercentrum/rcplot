616ccf050d53b6ffb98f21a92de94d665d28c183
Rikard Isaksson <rikard.isaksson@vgregion.se>
2022-03-24 09:07:27 +0100

    Removed columns from map data to make it smaller

--- R/swe_data.R
+++ R/swe_data.R
@@ -15,7 +15,7 @@
 #' # Note that coord_map() is essential for the map to be in actual scale.
 #'
 #' ggplot2::ggplot(
-#'   data = swe_landsting,
+#'   data = swe_coords_county,
 #'   ggplot2::aes(x=long, y=lat, group = group)
 #' ) +
 #' ggplot2::geom_polygon(color = "white", size = 0, fill = "grey")  +
@@ -58,7 +58,6 @@
 #'   plot.margin          = ggplot2::margin(0,0,0,0, unit = "cm")
 #' )
 #'
-#' @keywords county landsting region
 #' @name county_names
 "cnames1"
 
@@ -67,7 +66,6 @@
 #'
 #' @format A data frame with 12 rows and 3 variables.
 #'   The negligible variables have been removed from the original data set:
-#'
 #'   \describe{
 #'     \item{NAME_1}{name of county, character}
 #'     \item{long}{longitude, numeric}
@@ -80,7 +78,7 @@
 #' # Note that coord_map() is essential for the map to be in actual scale.
 #'
 #' ggplot2::ggplot(
-#'   data = swe_landsting,
+#'   data = swe_coords_county,
 #'   ggplot2::aes(x=long, y=lat, group = group)
 #' ) +
 #' ggplot2::geom_polygon(
@@ -125,14 +123,15 @@
 #'   legend.justification = c(0,1),
 #'   plot.margin          = ggplot2::margin(0,0,0,0, unit = "cm")
 #' )
-#' @keywords county landsting region
+#'
+#'
 #' @name county_names
 "cnames2"
 
 #' Data set used to translate ID code to name
 #'
-#' @format A data frame with 311 rows and 3 variables.
 #'
+#' @format A data frame with 311 rows and 3 variables:
 #'   \describe{
 #'     \item{ColumnName}{name of variable, character}
 #'     \item{ValueCode}{ID, character}
@@ -141,16 +140,18 @@
 #'
 #' @examples
 #'
-#' region_names <- dplyr::filter(swe_value_labels, ColumnName == "Region") %>%
-#' dplyr::select(Region = ValueCode, RegionName = ValueName)
+#' # Example on how to add Region names to coordinates map data
 #'
-#' df <- left_join(
+#' region_names <- dplyr::filter(swe_value_labels, ColumnName == "Region")
+#'
+#' region_names <- dplyr::select(region_names, Region = ValueCode, RegionName = ValueName)
+#'
+#' df <- dplyr::left_join(
 #'   swe_coords_municipality,
 #'   region_names,
 #'   by = c("Region")
 #' )
 #'
-#' @keywords county landsting region
 #' @name county_names
 "swe_value_labels"
 
@@ -160,17 +161,13 @@
 #' @format A data frame with 11488 rows and 9 variables. This is a filtered
 #'   version of swe_kommuner_allpoints where all points with piece equal to "3"
 #'   or larger are removed (small details) and only every thirtieth point being
-#'   used (see examples).
+#'   used (see examples):
 #'   \describe{
 #'     \item{long}{longitude, numeric}
 #'     \item{lat}{latitude, numeric}
-#'     \item{order}{specifies the order for each
-#'                  point, integer}
-#'     \item{piece}{"1" for the most essential, "2" and more for
-#'                  detailed points, factor}
-#'     \item{group}{Each region or island in the map is a
-#'                  polygon where each level in this variable
-#'                  is a polygon, factor}
+#'     \item{order}{specifies the order for each point, integer}
+#'     \item{piece}{"1" for the most essential, "2" and more for detailed points, factor}
+#'     \item{group}{Each region or island in the map is a polygon where each level in this variable is a polygon, factor}
 #'     \item{Region}{id of county, integer}
 #'     \item{Kommun}{id of township, integer}
 #'   }
@@ -185,33 +182,26 @@
 #'   ggplot2::coord_map() +
 #'   ggplot2::theme_minimal()
 #'
-#' @keywords municipality kommuner map
 #' @name sweden_map
 "swe_coords_municipality"
 
 #' Sweden map data set with county included, compressed version
 #'
+#'
 #' @format A data frame with 7,791 rows and 19 variables. This is a filtered
 #'   version of swe_landsting_allpoints where all points with piece equal to "3"
 #'   or larger are removed (small details) and only every thirtieth point being
 #'   used (see examples). Also some example variables are included for example
-#'   purposes, calculated for 2016 annual report.
-#'
+#'   purposes, calculated for 2016 annual report:
 #'   \describe{
-#'     \item{long          }{longitude, numeric}
-#'     \item{lat           }{latitude, numeric}
-#'     \item{order         }{specifies the order for each point, integer}
-#'     \item{piece         }{"1" for the most essential, "2" and more for
-#'                           detailed points (Öland, Orust, and Tjörn included
-#'                           in "2"), factor}
-#'     \item{group         }{Each region or island in the map is a polygon where
-#'     \item{Region        }{id of county, character}
+#'     \item{long  }{longitude, numeric}
+#'     \item{lat   }{latitude, numeric}
+#'     \item{order }{specifies the order for each point, integer}
+#'     \item{piece }{"1" for the most essential, "2" and more for detailed points (Öland, Orust, and Tjörn included in "2"), factor}
+#'     \item{group }{Each region or island in the map is a polygon where}
+#'     \item{Region}{id of county, character}
 #'  }
 #'
-#'  * Factor variable with 3 levels for counties compared to the interval
-#'  \eqn{(\mu - \sigma, \mu + \sigma} where \eqn{\mu} and \eqn{\sigma} are the
-#'  mean and standard deviation for whole Sweden:
-#'   bad (below the interval), average (within) and good (above).
 #'
 #' @examples
 #'
@@ -270,6 +260,7 @@
 #'   legend.justification = c(0,1),
 #'   plot.margin          = ggplot2::margin(0,0,0,0, unit = "cm")
 #' )
-#' @keywords county landsting region map
+#'
+#'
 #' @name sweden_map
 "swe_coords_county"
