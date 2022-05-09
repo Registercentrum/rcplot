a80d934105db8e31f015c925130dd4f5b5d5ad3c
Rikard Isaksson <rikard.isaksson@vgregion.se>
2022-05-09 16:42:53 +0200

    update documentation for coordinates datasets

--- R/swe_data.R
+++ R/swe_data.R
@@ -2,44 +2,31 @@
 #'
 #' @format A sf dataset with one row per county
 #' \describe{
-#'   \item{id}{ID of county}
+#'   \item{id}{ID of county (as in data from SCB)}
 #'   \item{geometry}{Coordinate polygon used to draw county borders}
+#'   \item{Name}{Name of county}
 #' }
 #'
-#' @examples
-#' library(sf)
-#' library(ggplot2)
-#' ggplot() +
-#'  geom_sf(data = counties, aes(fill = as.numeric(id))) +
-#'  theme_void()
+#' @example man/examples/map_plot.R
 #'
-#' ggplot() +
-#'   geom_sf(data = municipalities, aes(fill = as.numeric(id))) +
-#'   geom_sf(data = counties, fill = NA, color = "red") +
-#'  theme_void()
-#'
-#' @source \url{http://api.thenmap.net/v2/se-4/geo/2020-06-03}
+#' @source
+#'   \url{http://api.thenmap.net/v2/se-4/geo/2020-06-03}
+#'   \url{"https://www.scb.se/contentassets/7a89e48960f741e08918e489ea36354a/kommunlankod_20211229.xls"}
 "counties"
 
 #' Map coordinates for Sweden municipalites
 #'
 #' @format A sf dataset with one row per municipality
 #' \describe{
-#'   \item{id}{ID of municipality}
+#'   \item{id}{ID of municipality with region code (as in data from SCB)}
 #'   \item{geometry}{Coordinate polygon used to draw municipality borders}
+#'   \item{Name}{Name of municipality}
+#'   \item{RegionID}{County code}
+#'   \item{MunicipID}{Municipality code}
 #' }
-#' @examples
-#' library(sf)
-#' library(ggplot2)
+#' @example man/examples/map_plot.R
 #'
-#' ggplot() +
-#'  geom_sf(data = municipalities, aes(fill = as.numeric(id))) +
-#'  theme_void()
-#'
-#' ggplot() +
-#'   geom_sf(data = municipalities, aes(fill = as.numeric(id))) +
-#'   geom_sf(data = counties, fill = NA, color = "red") +
-#'  theme_void()
-#'
-#' @source \url{http://api.thenmap.net/v2/se-7/geo/2020-06-03}
+#' @source
+#'   \url{http://api.thenmap.net/v2/se-7/geo/2020-06-03}
+#'   \url{"https://www.scb.se/contentassets/7a89e48960f741e08918e489ea36354a/kommunlankod_20211229.xls"}
 "municipalities"
